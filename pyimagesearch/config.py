4010ce46791a8cbf9667a1c4b71408300933dfbf
Sayak <spsayakpaul@gmail.com>
2019-12-08 05:31:52 +0000

    Adding updated notebooks

---
+++ pyimagesearch/config.py
@@ -0,0 +1,11 @@
+# import the necessary packages
+import os
+
+# define the minimum learning rate, maximum learning rate, batch size,
+# step size, CLR method, and number of epochs
+MIN_LR = 1e-5
+MAX_LR = 1e-3
+BATCH_SIZE = 32
+STEP_SIZE = 8
+CLR_METHOD = "triangular"
+NUM_EPOCHS = 20
